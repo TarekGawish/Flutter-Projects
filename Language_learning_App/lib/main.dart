22e0963ea39bd997952f3638091532687b99692c
tarek gawish <tarek29gawesh55@gmail.com>
2023-12-09 23:06:41 +0200

    add another sections to the app

--- Language_learning_App/lib/main.dart
+++ Language_learning_App/lib/main.dart
@@ -27,9 +27,42 @@ class LanguageLearnignApp extends StatelessWidget {
             width: double.infinity,
             child: Text(
               'Numbers',
-              style: TextStyle(color: Color(0xffffffff), fontSize: 18),
+              style: TextStyle(color: Color(0xff000000), fontSize: 18),
             ),
-          )
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            color: Color(0xffF9F9E0),
+            height: 50,
+            width: double.infinity,
+            child: Text(
+              'Family Members',
+              style: TextStyle(color: Color(0xff000000), fontSize: 18),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            color: Color(0xffFFC0D9),
+            height: 50,
+            width: double.infinity,
+            child: Text(
+              'Colors',
+              style: TextStyle(color: Color(0xff000000), fontSize: 18),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            color: Color(0xff8ACDD7),
+            height: 50,
+            width: double.infinity,
+            child: Text(
+              'Phrases',
+              style: TextStyle(color: Color(0xff000000), fontSize: 18),
+            ),
+          ),
         ]),
       ),
     );
